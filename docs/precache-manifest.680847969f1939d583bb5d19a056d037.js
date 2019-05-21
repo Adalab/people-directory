cbe1abb6ee8d724d5af11d4c7086090d1fa69ee1
Nasiba Carrasco <nasivuela@gmail.com>
2019-05-21 14:42:06 +0200

    Publish

---
+++ docs/precache-manifest.680847969f1939d583bb5d19a056d037.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "deec77c139205ccd54f609430537f6b8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2bca524c8fed1711fb99",
+    "url": "./static/css/main.03c5b705.chunk.css"
+  },
+  {
+    "revision": "2310902dea5dc0625e05",
+    "url": "./static/js/2.b9f93adb.chunk.js"
+  },
+  {
+    "revision": "2bca524c8fed1711fb99",
+    "url": "./static/js/main.d90a9c6c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
